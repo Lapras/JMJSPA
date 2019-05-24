b9b68eae8278f95293f4406fa9e868cb356d3e5e
Lapras <45187326+Lapras@users.noreply.github.com>
2019-05-24 09:41:15 -0400

    Add files via upload

---
+++ Javascript.js
@@ -0,0 +1,21 @@
+var answers = [
+    'Your question confuses me, mortal.',
+    'No, you fool, this is false',
+    'I am not sure, this is larger than me',
+    'Umm... I dont know man, this is your problem.',
+    'Good question, I dont know the answer',
+    'OK, that was wierd, you can go away now',
+    'This is probably true',
+    'Ehhh, its possible, but maybe not.',
+    'This is probably true',
+    'This is probably true',
+    'Ehhh, its possible, but maybe not.',
+    'This is most definetely true, yet insignificant.',
+    'Nope, no way.',
+    'No.'
+];
+function answer() {
+    prompt('Ask the 8 ball you question!', 'question');
+    let randNum = Math.floor(Math.random() * answers.length);
+    document.getElementById('display').innerHTML = answers[randNum];
+}
